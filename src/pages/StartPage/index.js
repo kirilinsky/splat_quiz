b7bcab60d85d60ecb6feb214a76d82105ddb2dd7
kirilinsky <kirill.ilinsky@gmail.com>
2023-03-23 22:29:15 +0100

    change start page

--- src/pages/StartPage/index.js
+++ src/pages/StartPage/index.js
@@ -3,53 +3,69 @@ import styles from "./style.module.css";
 import { useDispatch } from "react-redux";
 import { secondQuiz } from "../../actions/routingApp";
 import Title from "../../components/Title";
+import Tilt from 'react-parallax-tilt';
+
+import './style.scss'
+/* asset */
+import bg from './assets/bg.png';
+import tooth from './assets/tooth.png';
+import disc from './assets/disc.png';
 
 const StartPage = () => {
   const dispatch = useDispatch();
   return (
     <>
-      <Title text="Оценка состояния полости рта" />
-      <div className="quiz-wrapper">
-        <div className="container">
-          <div className={styles.hero}>
-            <div className={styles.hero__text}>
-              <div className={styles.hero__line}>
-                <span></span>
-              </div>
-              <div className={styles.hero__title}>
-                <h2>
-                  РАССКАЖИТЕ, ЧТО ВАС БЕСПОКОИТ, ПОЛУЧИТЕ РЕКОМЕНДАЦИИ И
-                  ПРОКОНСУЛЬТИРУЙТЕСЬ СО СТОМАТОЛОГОМ, ОДОБРЕННЫМ SPLAT GLOBAL
-                </h2>
-              </div>
-              <div className={styles.hero__button}>
-                <button
-                  onClick={() => {
-                    window.scrollTo(0, 0);
-                    dispatch(secondQuiz());
-                  }}
-                >
-                  Начать
-                </button>
-              </div>
-              <div className={styles.hero__description}>
-                <p>
-                  Дифференциально-диагностический опросник разработан совместно
-                  с Вице-президентом "СтАР", доктором медицинских наук, врачем
-                  высшей категории Кузнецовым Сергеем Владимировичем. Тест
-                  построен на основе клинических рекомендаций Стоматологической
-                  ассоциации России (СтАР) e-stomatology.ru, не является
-                  средством постановки диагноза и советом по медицинскому
-                  вопросу. По всем вопросам, имеющим отношение к состоянию
-                  здоровья, врачебной и медицинской помощи, необходимо
-                  проконсультироваться со специалистом
-                </p>
+      {/* <Title text="Оценка состояния полости рта" /> */}
+
+      <div className="mainscreen_grid">
+        <div className="mainscreen_content">
+          <h1>Оценка состояния полости рта</h1>
+          <p>Расскажите, что вас беспокоит, получите рекомендации и проконсультируйтесь
+            со стоматологом, одобренным Splat Global</p>
+          
+            <button
+              onClick={() => {
+                window.scrollTo(0, 0);
+                dispatch(secondQuiz());
+              }}
+            >
+              Начать
+            </button>
+        
+        </div>
+        <div className="mainscreen_picture">
+          <div className="hero_desktop">
+            <div className="hero_desktop_wrapper">
+              <img className="desktop" alt="circle" src={bg} />
+              <div className="hero_desktop_parallax">
+                <Tilt className="hero_desktop_shadow" tiltMaxAngleX={30}
+                  tiltMaxAngleY={22}
+                  perspective={800}
+                  transitionSpeed={1500}
+                  gyroscope={true}>
+                  <img className="tooth" src={tooth} alt="tooth" />
+                </Tilt>
               </div>
             </div>
-            <div className={styles.hero__image}></div>
+          </div>
+
+        </div>
+        <div className="mainscreen_disc">
+          <div className="mainscreen_disc_wrap">
+            <img src={disc} alt="зубам полезно" />
+
+            <p>Данный опросник не является средством постановки диагноза, не является советом по медицинскому вопросу. Информация в опроснике не может быть использована для назначения лечения и не заменяет прием врача. По всем вопросам, имеющим отношение к состоянию здоровья, врачебной и медицинской помощи, необходимо проконсультироваться со специалистом
+              <br />
+              <br />
+              Опросник разработан при поддержке Президента Профессионального общества гигиенистов стоматологических</p>
+
           </div>
         </div>
+
       </div>
+
+
+
     </>
   );
 };
