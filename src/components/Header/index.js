398daa7bee5e107761b32b913012aa439b42ce7b
kirilinsky <kirill.ilinsky@gmail.com>
2023-03-29 23:47:33 +0200

    logo fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header>
       <div className={styles.header}>
-        <a href="https://promo.splatglobal.com/diagnostics/" className={styles.header__logo}>
+        <a href="/" className={styles.header__logo}>
           <img src={logo} alt="spalt" />
         </a>
       </div>
