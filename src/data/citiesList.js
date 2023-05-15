a7fe849bdc317225854f6683827ad57b6fa8df63
kirilinsky <kirill.ilinsky@gmail.com>
2023-05-15 11:22:48 +0200

    add regions

--- src/data/citiesList.js
+++ src/data/citiesList.js
@@ -1,46 +1,394 @@
+
+
 export const listOfCities = [
-  { id: 43, region: 'Москва', fluorine: false },
-  { id: 44, region: 'Санкт-Петербург', fluorine: false },
-  { id: 1, region: 'Республика Адыгея', fluorine: false},
-  { id: 2, region: 'Архангельская область', fluorine: false},
-  { id: 3, region: 'Астраханская область', fluorine: false },
-  { id: 4, region: 'Республика Башкортостан' , fluorine: false},
-  { id: 5, region: 'Брянская область' , fluorine: false},
-  { id: 6, region: 'Республика Бурятия' , fluorine: false},
-  { id: 7, region: 'Волгоградская область' , fluorine: false},
-  { id: 8, region: 'Воронежская область' , fluorine: false},
-  { id: 9, region: 'Республика Дагестан' , fluorine: false},
-  { id: 10, region: 'Республика Ингушетия' , fluorine: false},
-  { id: 11, region: 'Иркутская область', fluorine: false },
-  { id: 12, region: 'Республика Кабардино-Балкария' , fluorine: false},
-  { id: 13, region: 'Республика Карелия' , fluorine: false},
-  { id: 14, region: 'Кемеровская область', fluorine: false },
-  { id: 15, region: 'Краснодарский край' , fluorine: false},
-  { id: 16, region: 'Красноярский край' , fluorine: false},
-  { id: 17, region: 'Курская область' , fluorine: false},
-  { id: 18, region: 'Липецкая область' , fluorine: false},
-  { id: 19, region: 'Московская область' , fluorine: false},
-  { id: 20, region: 'Мурманская область' , fluorine: false},
-  { id: 21, region: 'Нижегородская область' , fluorine: false},
-  { id: 22, region: 'Новосибирская область' , fluorine: false},
-  { id: 23, region: 'Омская область', fluorine: false },
-  { id: 24, region: 'Пензенская область' , fluorine: false},
-  { id: 25, region: 'Приморский край' , fluorine: false},
-  { id: 26, region: 'Ростовская область' , fluorine: false},
-  { id: 27, region: 'Самарская область' , fluorine: false},
-  { id: 28, region: 'Сахалинская область' , fluorine: false},
-  { id: 29, region: 'Свердловская область' , fluorine: false},
-  { id: 30, region: 'Смоленская область' , fluorine: false},
-  { id: 31, region: 'Республика Татарстан' , fluorine: false},
-  { id: 32, region: 'Тверская область' , fluorine: false},
-  { id: 33, region: 'Томская область' , fluorine: false},
-  { id: 34, region: 'Тульская область' , fluorine: false},
-  { id: 35, region: 'Республика Тыва' , fluorine: false},
-  { id: 36, region: 'Республика Удмуртия' , fluorine: false},
-  { id: 37, region: 'Хабаровский Край' , fluorine: false},
-  { id: 38, region: 'Ханты-Мансийский АО' , fluorine: false},
-  { id: 39, region: 'Челябинская область' , fluorine: false},
-  { id: 40, region: 'Читинская область' , fluorine: false},
-  { id: 41, region: 'Ярославская область' , fluorine: false},
-  { id: 42, region: 'Ленинградская область' , fluorine: false},
+  {
+    "id": 80,
+    fluorine: false, "region": "Москва",
+    "district_id": 1
+  },
+  {
+    "id": 81,
+    fluorine: false, "region": "Санкт-Петербург и область",
+    "district_id": 3
+  },
+  {
+    "id": 2,
+    fluorine: false, "region": "Республика Адыгея",
+    "district_id": 2
+  },
+  {
+    "id": 3,
+    fluorine: false, "region": "Алтайский край",
+    "district_id": 5
+  },
+  {
+    "id": 4,
+    fluorine: false, "region": "Амурская область",
+    "district_id": 4
+  },
+  {
+    "id": 5,
+    fluorine: false, "region": "Архангельская область",
+    "district_id": 3
+  },
+  {
+    "id": 6,
+    fluorine: false, "region": "Астраханская область",
+    "district_id": 2
+  },
+  {
+    "id": 7,
+    fluorine: false, "region": "Республика Башкортостан",
+    "district_id": 7
+  },
+  {
+    "id": 8,
+    fluorine: false, "region": "Белгородская область",
+    "district_id": 1
+  },
+  {
+    "id": 9,
+    fluorine: false, "region": "Брянская область",
+    "district_id": 1
+  },
+  {
+    "id": 10,
+    fluorine: false, "region": "Республика Бурятия",
+    "district_id": 5
+  },
+  {
+    "id": 11,
+    fluorine: false, "region": "Владимирская область",
+    "district_id": 1
+  },
+  {
+    "id": 12,
+    fluorine: false, "region": "Волгоградская область",
+    "district_id": 2
+  },
+  {
+    "id": 13,
+    fluorine: false, "region": "Вологодская область",
+    "district_id": 3
+  },
+  {
+    "id": 14,
+    fluorine: false, "region": "Воронежская область",
+    "district_id": 1
+  },
+  {
+    "id": 15,
+    fluorine: false, "region": "Республика Дагестан",
+    "district_id": 8
+  },
+  {
+    "id": 16,
+    fluorine: false, "region": "Еврейская АО",
+    "district_id": 4
+  },
+  {
+    "id": 17,
+    fluorine: false, "region": "Ивановская область",
+    "district_id": 1
+  },
+  {
+    "id": 18,
+    fluorine: false, "region": "Иркутская область",
+    "district_id": 5
+  },
+  {
+    "id": 19,
+    fluorine: false, "region": "Кабардино-Балкарская Республика",
+    "district_id": 8
+  },
+  {
+    "id": 20,
+    fluorine: false, "region": "Калининградская область",
+    "district_id": 3
+  },
+  {
+    "id": 21,
+    fluorine: false, "region": "Республика Калмыкия",
+    "district_id": 2
+  },
+  {
+    "id": 22,
+    fluorine: false, "region": "Калужская область",
+    "district_id": 1
+  },
+  {
+    "id": 23,
+    fluorine: false, "region": "Камчатская область",
+    "district_id": 4
+  },
+  {
+    "id": 24,
+    fluorine: false, "region": "Республика Карелия",
+    "district_id": 3
+  },
+  {
+    "id": 25,
+    fluorine: false, "region": "Кемеровская область",
+    "district_id": 5
+  },
+  {
+    "id": 26,
+    fluorine: false, "region": "Кировская область",
+    "district_id": 7
+  },
+  {
+    "id": 27,
+    fluorine: false, "region": "Республика Коми",
+    "district_id": 3
+  },
+  {
+    "id": 28,
+    fluorine: false, "region": "Костромская область",
+    "district_id": 1
+  },
+  {
+    "id": 29,
+    fluorine: false, "region": "Краснодарский край",
+    "district_id": 2
+  },
+  {
+    "id": 30,
+    fluorine: false, "region": "Красноярский край",
+    "district_id": 5
+  },
+  {
+    "id": 31,
+    fluorine: false, "region": "Курганская область",
+    "district_id": 6
+  },
+  {
+    "id": 32,
+    fluorine: false, "region": "Курская область",
+    "district_id": 1
+  },
+  {
+    "id": 33,
+    fluorine: false, "region": "Липецкая область",
+    "district_id": 1
+  },
+  {
+    "id": 34,
+    fluorine: false, "region": "Магаданская область",
+    "district_id": 4
+  },
+  {
+    "id": 35,
+    fluorine: false, "region": "Республика Марий Эл",
+    "district_id": 7
+  },
+  {
+    "id": 36,
+    fluorine: false, "region": "Республика Мордовия",
+    "district_id": 7
+  },
+  {
+    "id": 37,
+    fluorine: false, "region": "Мурманская область",
+    "district_id": 3
+  },
+  {
+    "id": 38,
+    fluorine: false, "region": "Нижегородская область",
+    "district_id": 7
+  },
+  {
+    "id": 39,
+    fluorine: false, "region": "Новгородская область",
+    "district_id": 3
+  },
+  {
+    "id": 40,
+    fluorine: false, "region": "Новосибирская область",
+    "district_id": 5
+  },
+  {
+    "id": 41,
+    fluorine: false, "region": "Омская область",
+    "district_id": 5
+  },
+  {
+    "id": 42,
+    fluorine: false, "region": "Оренбургская область",
+    "district_id": 7
+  },
+  {
+    "id": 43,
+    fluorine: false, "region": "Орловская область",
+    "district_id": 1
+  },
+  {
+    "id": 44,
+    fluorine: false, "region": "Пензенская область",
+    "district_id": 7
+  },
+  {
+    "id": 45,
+    fluorine: false, "region": "Пермский край",
+    "district_id": 7
+  },
+  {
+    "id": 46,
+    fluorine: false, "region": "Приморский край",
+    "district_id": 4
+  },
+  {
+    "id": 47,
+    fluorine: false, "region": "Псковская область",
+    "district_id": 3
+  },
+  {
+    "id": 48,
+    fluorine: false, "region": "Ростовская область",
+    "district_id": 2
+  },
+  {
+    "id": 49,
+    fluorine: false, "region": "Рязанская область",
+    "district_id": 1
+  },
+  {
+    "id": 50,
+    fluorine: false, "region": "Самарская область",
+    "district_id": 7
+  },
+  {
+    "id": 51,
+    fluorine: false, "region": "Саратовская область",
+    "district_id": 7
+  },
+  {
+    "id": 52,
+    fluorine: false, "region": "Республика Саха (Якутия)",
+    "district_id": 4
+  },
+  {
+    "id": 53,
+    fluorine: false, "region": "Сахалинская область",
+    "district_id": 4
+  },
+  {
+    "id": 54,
+    fluorine: false, "region": "Свердловская область",
+    "district_id": 6
+  },
+  {
+    "id": 55,
+    fluorine: false, "region": "Северная Осетия-Алания",
+    "district_id": 8
+  },
+  {
+    "id": 56,
+    fluorine: false, "region": "Смоленская область",
+    "district_id": 1
+  },
+  {
+    "id": 57,
+    fluorine: false, "region": "Ставропольский край",
+    "district_id": 8
+  },
+  {
+    "id": 58,
+    fluorine: false, "region": "Тамбовская область",
+    "district_id": 1
+  },
+  {
+    "id": 59,
+    fluorine: false, "region": "Республика Татарстан",
+    "district_id": 7
+  },
+  {
+    "id": 60,
+    fluorine: false, "region": "Тверская область",
+    "district_id": 1
+  },
+  {
+    "id": 61,
+    fluorine: false, "region": "Томская область",
+    "district_id": 5
+  },
+  {
+    "id": 62,
+    fluorine: false, "region": "Тувинская Республика",
+    "district_id": 5
+  },
+  {
+    "id": 63,
+    fluorine: false, "region": "Тульская область",
+    "district_id": 1
+  },
+  {
+    "id": 64,
+    fluorine: false, "region": "Тюменская область",
+    "district_id": 6
+  },
+  {
+    "id": 65,
+    fluorine: false, "region": "Республика Удмуртия",
+    "district_id": 7
+  },
+  {
+    "id": 66,
+    fluorine: false, "region": "Ульяновская область",
+    "district_id": 7
+  },
+  {
+    "id": 68,
+    fluorine: false, "region": "Хабаровский край",
+    "district_id": 4
+  },
+  {
+    "id": 69,
+    fluorine: false, "region": "Республика Хакасия",
+    "district_id": 5
+  },
+  {
+    "id": 70,
+    fluorine: false, "region": "Челябинская область",
+    "district_id": 6
+  },
+  {
+    "id": 71,
+    fluorine: false, "region": "Чечено-Ингушетия",
+    "district_id": 8
+  },
+  {
+    "id": 72,
+    fluorine: false, "region": "Забайкальский край",
+    "district_id": 5
+  },
+  {
+    "id": 73,
+    fluorine: false, "region": "Республика Чувашия",
+    "district_id": 7
+  },
+  {
+    "id": 74,
+    fluorine: false, "region": "Чукотский АО",
+    "district_id": 4
+  },
+  {
+    "id": 75,
+    fluorine: false, "region": "Ямало-Ненецкий АО",
+    "district_id": 6
+  },
+  {
+    "id": 76,
+    fluorine: false, "region": "Ярославская область",
+    "district_id": 1
+  },
+  {
+    "id": 77,
+    fluorine: false, "region": "Карачаево-Черкесская Республика",
+    "district_id": 8
+  },
+  {
+    "id": 78,
+    fluorine: false, "region": "Ханты-Мансийский АО",
+    "district_id": 6
+  }
 ]
